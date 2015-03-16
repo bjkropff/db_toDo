189c52469a6483d0497e91ed2c5e8ac7f071d53c
Bryan Borgeson & Brian Kropff <student@epicodus.com>
2015-03-16 15:39:55 -0700

    Add twig files to make front end usable.

--- app/app.php
+++ app/app.php
@@ -5,6 +5,8 @@
 
     $app = new Silex\Application();
 
+    $app['debug'] = TRUE;
+
     $DB = new PDO('pgsql:host=localhost;dbname=to_do');
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -12,9 +14,7 @@
     ));
 
     $app->get("/", function() use ($app) {
-
         return $app['twig']->render('index.twig');
-
     });
 
     $app->get("/tasks", function() use ($app) {
@@ -26,19 +26,25 @@
     });
 
     $app->post("/tasks", function() use ($app) {
-
         $task = new Task($_POST['description']);
         $task->save();
         return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
+    });
 
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app['twig']->render('index.twig');
     });
 
     $app->post("/delete_tasks", function() use ($app) {
-
         Task::deleteAll();
-
         return $app['twig']->render('index.twig');
-
     });
 
     return $app;
