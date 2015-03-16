9d647b71cb80f34b9eac4b44bbe4ba0be59473ea
Bryan Borgeson & Brian Kropff <student@epicodus.com>
2015-03-16 14:59:45 -0700

    Add silex functionality. Add get and post to app.php. Twig files to be added.

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
 
     $app = new Silex\Application();
 
@@ -12,16 +13,23 @@
 
     $app->get("/", function() use ($app) {
 
-        return $app['twig']->render('tasks_template.php', array('tasks' => Task::getAll()));
+        return $app['twig']->render('index.twig');
 
+    });
+
+    $app->get("/tasks", function() use ($app) {
+        return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
+    });
+
+    $app->get("/categories", function() use ($app){
+        return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
     });
 
     $app->post("/tasks", function() use ($app) {
 
         $task = new Task($_POST['description']);
         $task->save();
-
-        return $app['twig']->render('create_task.php', array('newtask' => $task));
+        return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
 
     });
 
@@ -29,7 +37,7 @@
 
         Task::deleteAll();
 
-        return $app['twig']->render('delete_task.php');
+        return $app['twig']->render('index.twig');
 
     });
 
