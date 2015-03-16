f0eb609e4d78e6d5fdbcc3344aba457a98b8f196
Bryan Borgeson & Brian Kropff <student@epicodus.com>
2015-03-16 12:13:05 -0700

    Add get and set methods, instantiating out Task objects with 's.

--- src/Task.php
+++ src/Task.php
@@ -2,10 +2,24 @@
     class Task
     {
         private $description;
+        private $id;
 
-        function __construct($description)
+        function __construct($description, $id = null)
         {
             $this->description = $description;
+            if($id !== null) {
+                $this->id = $id;
+            }
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setId($new_id)
+        {
+            $this->id = (int) $new_id;
         }
 
         function setDescription($new_description)
@@ -20,17 +34,24 @@
 
         function save()
         {
-            array_push($_SESSION['list_of_tasks'], $this);
+            $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
         }
 
         static function getAll()
         {
-            return $_SESSION['list_of_tasks'];
+            $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+            $tasks = array();
+            foreach ($returned_tasks as $task) {
+                $description = $task['description'];
+                $new_task = new Task($description);
+                array_push($tasks, $new_task);
+            }
+            return $tasks;
         }
 
         static function deleteAll()
         {
-            $_SESSION['list_of_tasks'] = array();
+            $GLOBALS['DB']->exec("DELETE FROM tasks *;");
         }
     }
 ?>
